11ca0b1863c5a9c007cd7099aaf918c74cfc99ec
Mark <nuvazic@gmail.com>
2026-09-19 12:18:39 +0200

    v0.3.0: expose constrained CAD generator API

--- app/cad/__init__.py
+++ app/cad/__init__.py
@@ -1,9 +1,19 @@
+from .generator import (
+    BracketLayout,
+    build_pipe_saddle_bracket,
+    derive_layout,
+    export_bracket,
+)
 from .models import CADReleaseDecision, CADReleaseStatus, ReleasedBracketParameters
 from .release_gate import evaluate_cad_release
 
 __all__ = [
+    "BracketLayout",
     "CADReleaseDecision",
     "CADReleaseStatus",
     "ReleasedBracketParameters",
+    "build_pipe_saddle_bracket",
+    "derive_layout",
     "evaluate_cad_release",
+    "export_bracket",
 ]
